179382cde14e204e52c55c4d992679b58e67b56f
sagar3595 <sagar47kumar13@gmail.com>
2022-12-01 11:10:30 +0530

    bitcoin

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import{BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Header from './components/Header';
+import CoinDetails from './components/CoinDetails';
+import Coins from './components/Coins';
+import Exchanges from './components/Exchanges';
+import Home from './components/Home';
+import Footer from './components/Footer';
+
+
+
+function App() {
+  return (
+ <Router>
+  <Header />
+  <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/coins' element={<Coins/>} />
+    <Route path='/exchanges' element={<Exchanges/>} />
+    <Route path='/coin/:id' element={<CoinDetails/>} />
+  </Routes>
+
+  <Footer />
+ </Router>
+  );
+}
+
+export default App;
